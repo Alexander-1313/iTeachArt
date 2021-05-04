f0a964eaf6c813f66d466fc4f3a6ca33623a7c91
alexander <alexander-1313>
2021-05-04 09:39:28 +0300

    Initial commit

---
+++ src/main/java/com/iteachart/stock/IteachartApplication.java
@@ -0,0 +1,13 @@
+package com.iteachart.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteachartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IteachartApplication.class, args);
+    }
+
+}
