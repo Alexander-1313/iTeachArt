5704d2a517e014491e332c305780aceb31fcc457
alexander <alexander-1313>
2021-05-11 14:35:35 +0300

    update initialization sql file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
-insert into User_Role (id, role) values(1, 'ROLE_UER'), (2, 'ROlE_ADMIN');
+insert into User_Role (id, role) values(1, 'ROLE_USER'), (2, 'ROlE_ADMIN');
 insert into Subscribe (id, type, cost) values (1, 'PRO', 100), (2, 'PREMIUM', 200), (3, 'PREMIUM+', 250);
+insert into User (first_name, second_name, email, password, id_role, is_blocked) values('Alexander', 'Rybak', 'alexander.rybak2020@mail.ru', '123456', 2, false);
+insert into User (first_name, second_name, email, password, id_role, is_blocked) values('Kirill', 'Kohno', 'kohnik1101@gmail.com', '9876123', 1, true);
