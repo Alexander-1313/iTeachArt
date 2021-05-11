5abd9bdd8cef3c3829564464160c300a1c847c0a
alexander <alexander-1313>
2021-05-11 15:39:00 +0300

    add @slf4j and update method with block\unblock user

--- src/main/java/com/iteachart/stock/service/impl/UserServiceImpl.java
+++ src/main/java/com/iteachart/stock/service/impl/UserServiceImpl.java
@@ -6,56 +6,50 @@ import com.iteachart.stock.repository.SubscribeRepository;
 import com.iteachart.stock.repository.UserRepository;
 import com.iteachart.stock.service.UserService;
 import com.iteachart.stock.util.UserUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private static final Logger LOGGER = LogManager.getLogger(UserServiceImpl.class);
-    private UserRepository userRepository;
-    private SubscribeRepository subscribeRepository;
-
-    @Autowired
-    public UserServiceImpl(UserRepository userRepository, SubscribeRepository subscribeRepository) {
-        this.userRepository = userRepository;
-        this.subscribeRepository = subscribeRepository;
-    }
+    private final UserRepository userRepository;
+    private final SubscribeRepository subscribeRepository;
 
     @Override
     public void blockUser(String email) {
         User byEmail = userRepository.findByEmail(email);
-        if(byEmail != null){
-            byEmail.setIsBlocked(true);
-            userRepository.save(byEmail);
-            LOGGER.info("user with email = " + email + " was blocked");
-        }else{
-            LOGGER.info("user with email = " + email + " not found");
+        if(byEmail == null){
+            log.info("user with email={} not found", email);
+            return;
         }
+        byEmail.setIsBlocked(true);
+        userRepository.save(byEmail);
+        log.info("user with email={} was blocked", email);
     }
 
     @Override
     public void unblockUser(String email) {
         User byEmail = userRepository.findByEmail(email);
-        if(byEmail != null){
-            byEmail.setIsBlocked(false);
-            userRepository.save(byEmail);
-            LOGGER.info("user with email = " + email + " was unblocked");
-        }else{
-            LOGGER.info("user with email = " + email + " not found");
+        if(byEmail == null){
+            log.info("user with email={} not found", email);
+            return;
         }
+        byEmail.setIsBlocked(false);
+        userRepository.save(byEmail);
+        log.info("user with email={} was unblocked", email);
     }
 
     @Override
     public void changeSubscribe(String email, Subscribe subscribe) {
         User userByEmail = userRepository.findByEmail(email);
         if (userByEmail == null) {
-            LOGGER.info("user with email = " + email + " not found");
+            log.info("user with email={} not found", email);
         }else{
             userByEmail.setSubscribe(subscribe);
             userByEmail.setSubscribeExpireDate(LocalDate.now().plusDays(UserUtils.subscribeDuration));
@@ -67,7 +61,7 @@ public class UserServiceImpl implements UserService {
             userRepository.save(userByEmail);
             subscribeRepository.save(subscribeFromRepo);
 
-            LOGGER.info("subscribe was changed!");
+            log.info("subscribe was changed!");
         }
     }
 
@@ -75,11 +69,11 @@ public class UserServiceImpl implements UserService {
     public void updateSubscribe(String email) {
         User userByEmail = userRepository.findByEmail(email);
         if (userByEmail == null) {
-            LOGGER.info("user with email = " + email + " not found");
+            log.info("user with email={} not found", email);
         }else{
             userByEmail.setSubscribeExpireDate(LocalDate.now().plusDays(UserUtils.subscribeDuration));
             userRepository.save(userByEmail);
-            LOGGER.info("subscribe was updated!");
+            log.info("subscribe was updated!");
         }
     }
 
