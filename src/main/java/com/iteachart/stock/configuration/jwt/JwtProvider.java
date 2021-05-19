c9b06ff8d6717fa29fc90648803557cd1d011aaf
alexander <alexander-1313>
2021-05-19 16:35:40 +0300

    add jwt provider and filter

---
+++ src/main/java/com/iteachart/stock/configuration/jwt/JwtProvider.java
@@ -0,0 +1,49 @@
+package com.iteachart.stock.configuration.jwt;
+
+import com.iteachart.stock.util.StringConstant;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtProvider {
+
+
+    public String generateToken(String email) {
+        Date date = Date.from(LocalDate.now().plusDays(15).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(date)
+                .signWith(SignatureAlgorithm.HS512, StringConstant.JWT_SECREAT)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(StringConstant.JWT_SECREAT).parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException expEx) {
+            log.info("Token expired");
+        } catch (UnsupportedJwtException unsEx) {
+            log.info("Unsupported jwt");
+        } catch (MalformedJwtException mjEx) {
+            log.info("Malformed jwt");
+        } catch (SignatureException sEx) {
+            log.info("Invalid signature");
+        } catch (Exception e) {
+            log.info("invalid token");
+        }
+        return false;
+    }
+
+    public String getLoginFromToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(StringConstant.JWT_SECREAT).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+
+}
