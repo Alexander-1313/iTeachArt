0a1d7d7f67a35622dd76122222b2ebdd08bc224c
alexander <alexander-1313>
2021-05-19 16:24:08 +0300

    update CheckRoleController

--- src/main/java/com/iteachart/stock/controller/CheckRoleController.java
+++ src/main/java/com/iteachart/stock/controller/CheckRoleController.java
@@ -1,18 +1,24 @@
 package com.iteachart.stock.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
+@Slf4j
 public class CheckRoleController {
 
     @GetMapping("/user")
-    public String user(){
+    public String user(Principal principal){
+        log.info("user with email={}", principal.getName());
         return "user";
     }
 
     @GetMapping("/admin")
-    public String admin(){
+    public String admin(Principal principal){
+        log.info("user with email={}", principal.getName());
         return "admin!";
     }
 }
