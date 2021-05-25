aa7589157d016a878509705aff3a80dac21c18b4
alexander <alexander-1313>
2021-05-25 14:22:45 +0300

    add test for controllers

---
+++ src/test/java/com/iteachart/stock/controller/StockControllerTest.java
@@ -0,0 +1,92 @@
+package com.iteachart.stock.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDate;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser(roles = {"USER", "ADMIN"})
+    @Test
+    public void testAutoToUserControllerToGetCompanyData() throws Exception{
+        mvc.perform(get("/company").param("symbol", "AAPL").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(roles = {"USER", "ADMIN"})
+    @Test
+    public void testAutoToUserControllerToGetAllCompanies() throws Exception{
+        mvc.perform(get("/all").param("exchange", "US").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(roles = {"USER", "ADMIN"})
+    @Test
+    public void testAutoToUserControllerToGetFinancialReports() throws Exception{
+        mvc.perform(get("/financialReports").param("symbol", "AAPL").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(roles = {"USER", "ADMIN"})
+    @Test
+    public void testAutoToUserControllerToGetShares() throws Exception{
+        mvc.perform(get("/shares").param("symbol", "AAPL").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(roles = {"USER", "ADMIN"})
+    @Test
+    public void testAutoToUserControllerToGetNews() throws Exception{
+        MultiValueMap params = new LinkedMultiValueMap();
+        params.add("symbol", "AAPL");
+        params.add("from", LocalDate.now().minusDays(1).toString());
+        params.add("to", LocalDate.now().toString());
+        mvc.perform(get("/news").params(params)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(roles = {"USER", "ADMIN"})
+    @Test
+    public void testAutoToUserControllerToGetCandles() throws Exception{
+        MultiValueMap params = new LinkedMultiValueMap();
+        params.add("symbol", "AAPL");
+        params.add("from", LocalDate.now().minusDays(1).toString());
+        params.add("to", LocalDate.now().toString());
+        mvc.perform(get("/candles").params(params)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+}
